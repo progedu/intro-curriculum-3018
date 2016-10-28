c9068d3777b59a9576833fb1b441c9d1faff334b
mizut <mztns.net@gmail.com>
2016-10-28 12:28:40 +0000

    Cookieに有効期限を設定

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = new Date().getTime();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-  res.end(req.headers.cookie);
+  const exp = new Date('Mon, 07 Jan 2036 00:00:00 GMT');
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=' + exp.toGMTString() + ';');
+  res.end(req.headers.cookie + '; expires=' + exp.toISOString() + ';'); //ISOSTring => 2036-01-07T00:00:00.000Z
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
+
 
