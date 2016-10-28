97546602b9358d2499c0157d9d730d0da9e15512
davideryuorihara3 <davideryug@gmail.com>
2016-10-28 12:34:24 +0000

    Cookieに有効期限を設定

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  const now = new Date().getTime();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-  res.end(req.headers.cookie);
+  let accessed;
+  if (req.headers.cookie) {
+    accessed = Number(req.headers.cookie.split('=')[1]);
+  } else {
+    accessed = 0;
+  }
+  
+  if(req.url !== '/favicon.ico') {
+    accessed++;
+  }
+  res.setHeader('Set-Cookie', 'last_access=' + accessed + ';'+ 'expires=Mon, 07 Jan 2036 00:00:00 GMT;');
+  res.end('このページに ' + accessed + ' 回アクセスしました。');
 });
 const port = 8000;
 server.listen(port, () => {
