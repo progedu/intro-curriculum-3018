ec94459a57c6ad58717e73149f7fa1f44765fb98
Yukimasa Shimada <github@eleos.jp>
2016-10-31 08:28:41 +0000

    アクセスカウンターver

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  const now = new Date().getTime();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;');
-  res.end(req.headers.cookie);
+  if (req.url === '/') {
+    let accessed = req.headers.cookie ? req.headers.cookie.match(/\d+/g) : 0;
+      accessed++;
+    res.setHeader('Set-Cookie', 'last_access=' + accessed + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;');
+    res.end('このページに ' + accessed + ' 回アクセスしました。');
+  }
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
-
