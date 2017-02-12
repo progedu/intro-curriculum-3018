41d2d86129813273338abf2c08382925697a59f5
tksfj17 <tksfj17@gmail.com>
2017-02-12 14:02:58 +0000

    時刻を見やすくした

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   const now = new Date().getTime();
   res.setHeader('Set-Cookie', 'last_access=' + now + '; expires=Mon, 07 Jan 2036 00:00:00 GMT;');
-  res.end(req.headers.cookie);
+  const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+  res.end(new Date(last_access_time).toString());
 });
 const port = 8000;
 server.listen(port, () => {
