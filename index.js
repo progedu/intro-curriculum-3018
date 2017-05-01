a5a606495300e740d79813235f1a4967fd2d8f1b
TomoyukiAota <tomoyukia@gmail.com>
2017-05-01 18:46:04 +0000

    アクセスカウンタを実装してみた

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  const now = new Date().getTime();
-  res.setHeader(
-    'Set-Cookie',
-    'last_access=' + now + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;'
-  );
-  res.end(req.headers.cookie);
+  const cookieString = req.headers.cookie;
+  let accessCount = cookieString ? parseInt(cookieString.split('access_count=')[1]) : 0;
+  if(req.url !== '/favicon.ico') accessCount++;
+  res.setHeader('Set-Cookie', 'access_count=' + accessCount)
+  res.end('アクセス回数：' + accessCount.toString() + '回');
+
+  // const now = new Date().getTime();
+  // res.setHeader(
+  //   'Set-Cookie',
+  //   'last_access=' + now + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;'
+  // );
+  // res.end(req.headers.cookie);
 });
 const port = 8000;
 server.listen(port, () => {
