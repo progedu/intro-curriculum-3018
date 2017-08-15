30416e7f6fa9fb39095fc3769f29c77e56753e5c
yusuke_nakajima <www.yyworld@gmail.com>
2017-08-15 12:48:41 +0900

    コード整形

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  const now = new Date().getTime();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-  res.end(req.headers.cookie);
+    const now = new Date().getTime();
+    res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+    res.end(req.headers.cookie);
 });
 const port = 8000;
 server.listen(port, () => {
-  console.info('Listening on ' + port);
+    console.info('Listening on ' + port);
 });
 
