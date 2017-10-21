6ee2587722d53aaf8e944e358b8a3648f9f0a1ce
MISAKO OHATA <misa@ohatadesign.com>
2017-10-21 09:41:07 +0000

    add cookie to expires

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  const now = new Date().getTime();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+  //const now = new Date().getTime();
+   const dt = new Date(2036, 0, 7, 0, 0);
+  res.setHeader('Set-Cookie', 'expires=' + dt + ';');
   res.end(req.headers.cookie);
 });
 const port = 8000;
