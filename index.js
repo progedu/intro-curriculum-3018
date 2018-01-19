fc2fc5170031a505656593c5be1e4ce5488c5288
orifuse <mitsube+github@gmail.com>
2018-01-20 04:17:13 +0900

    ミリ秒表記を変換して時刻を見やすく

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const server = http.createServer((req, res) => {
   const now = new Date().getTime();
   const expires = 'Mon, 07 Jan 2036 00:00:00 GMT';
   res.setHeader('Set-Cookie', 'last_access=' + now + '; expires=' + expires + ';');
-  res.end(req.headers.cookie);
+  const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+  res.end(new Date(last_access_time).toString());
 });
 const port = 8000;
 server.listen(port, () => {
