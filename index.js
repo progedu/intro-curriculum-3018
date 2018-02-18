820938d786fb6c10b8ce5b063eca23aede78d9c8
Kosuke Kitazawa <nyasu0123@gmail.com>
2018-02-18 01:46:33 +0000

    add expires

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   const expire = new Date(2036,0,7);
   res.setHeader('Set-Cookie', 'expires=' + expire + ';');
-  res.end('expire=' + expire);
+  res.end('expires=' + expire);
 });
 const port = 8000;
 server.listen(port, () => {
