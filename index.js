a5a8035e58230e71b2a6492dd322e7c284270dec
hnsky <hnskyyy@gmail.com>
2018-03-11 09:04:35 +0000

    addtoexpirationdate

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = new Date().getTime();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;');
   res.end(req.headers.cookie);
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
-
