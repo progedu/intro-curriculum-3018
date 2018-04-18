39622682f5a326cfd65d96b5f34f1f4c4e04ed49
gkism <k510hai@gmail.com>
2018-04-18 01:33:11 +0000

    有効期限の追加

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   const now = new Date().getTime();
   res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-  res.end(req.headers.cookie);
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;');
 });
 const port = 8000;
 server.listen(port, () => {
