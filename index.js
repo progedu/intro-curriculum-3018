f30598b635c12c1aaee7920abce47ffb338f31ba
heha-proglea <heha965+proglea@gmail.com>
2018-10-28 18:32:25 +0000

    update: 最終アクセス日時をブラウザに表示する

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const server = http.createServer((req, res) => {
   const now = new Date().getTime();
   // Cookieデータのセット
   res.setHeader('Set-Cookie', `last_access=${now};`);
-  res.end(req.headers.cookie);
+  
+  // Cookieをreq.headers.cookieで取得し、取得できた場合はそれを、できなかった場合はnowを使う
+  // ※ sample: "last_access=1452147999931".split('last_access=') => ["", "1452147999931"]
+  const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+  res.end(new Date(last_access_time).toString());
 });
 const port = 8000;
 server.listen(port, () => {
