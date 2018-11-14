244f024aaa068340e17252ddc48357921d2bc0ce
nlnlnul <walking_5km@yahoo.co.jp>
2018-11-14 11:49:29 +0000

    Cookie の 有効期限を設定

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = new Date().getTime();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-  res.end(req.headers.cookie);
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;');
+  const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+  res.end(new Date(last_access_time).toString());
 });
-const port = 8000;
+const port = 8001;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
