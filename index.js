ed41f8b89a5d7b55b835e5e139029095c4cee24a
mugimeshi <mugimeishi@gmail.com>
2019-01-20 19:51:18 +0000

    add cookie expire date

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 const http = require('http');
+const expiresdate = new Date('Mon, 07 Jan 2036 00:00:00 GMT;');
+console.info(expiresdate);
 const server = http.createServer((req, res) => {
   const now = new Date().getTime();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+  res.setHeader('Set-Cookie', 'last_access=' + now + '; expires=' + expiresdate);
   res.end(req.headers.cookie);
 });
 const port = 8000;
