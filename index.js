5621ae2c42eaca45e55af9e480de3b78e49cf0a3
suzaku0607 <s.yukai1567@gmail.com>
2019-09-29 00:03:05 +0900

    有効期限の設定

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-<<<<<<< HEAD
-  const now = Date.now();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-=======
   const now = new Date().getTime();
   res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;');
->>>>>>> master
   res.end(req.headers.cookie);
 });
 const port = 8000;
