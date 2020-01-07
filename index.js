b0624b4063c6781ee9aac36cfc3b0295b85f61ee
SasuraiNoHoge <tanakanoobenjoygame@gmail.com>
2020-01-07 21:39:57 +0900

    Cookieに有効期限を設定

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;');
+  //const last_access = req.headers.cookie ? req.headers.cooki.split('lastaccess=')[1] : now;
+  //console.info(req.headers);
   res.end(req.headers.cookie);
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
-
