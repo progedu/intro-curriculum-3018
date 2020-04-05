bb4969bc147b31b1280bab62c1d20fb195d4eb78
Sato Hikaru <sugarlight1@gmail.com>
2020-04-05 15:08:23 +0900

    cookieに有効期限を設定

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,12 +1,13 @@
 'use strict';
-const http = require('http');
-const server = http.createServer((req, res) => {
-  const now = new Date().getTime();
+
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  var now = new Date().getTime();
   res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=Mon,07 Jan 2036 00:00:00 GMT;');
   res.end(req.headers.cookie);
 });
-const port = 8000;
-server.listen(port, () => {
+var port = 8000;
+server.listen(port, function () {
   console.info('Listening on ' + port);
 });
-
