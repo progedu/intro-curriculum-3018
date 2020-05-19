44e061a9815ae1e4eee376c8e0c461dab639220c
hytotk <hytotk@gmail.com>
2020-05-19 23:54:03 +0900

    Cookie の 有効期限を 2036 年の 1 月 7 日に設定

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=' + new Date(2036, 0, 7) + ';');
   res.end(req.headers.cookie);
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
-
