7fe6803c8f1cbca7cfa674366cca22031f789266
nakako <nakako.soft@gmail.com>
2020-05-21 03:00:27 +0900

    Cookieの有効期限を設定

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+<<<<<<< Updated upstream
   const now = Date.now();
   res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+=======
+  const now = new Date().getTime();
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';expires=Mon, 07 Jan 2036 00:00:00 GMT;');
+>>>>>>> Stashed changes
   res.end(req.headers.cookie);
 });
 const port = 8000;
