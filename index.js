6c0636ced4988df3a565a337e77147c0423ace0f
bcn <bcn@BCN-MacBook-Air-2.local>
2020-06-03 23:27:42 +0900

    cookieの有効期限を追加

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  const now = Date.now();
+  const now = new Date();
+
   res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+  res.setHeader('expires', 'Mon, 07 Jan 2036 00:00:00 GMT;');
   res.end(req.headers.cookie);
 });
 const port = 8000;
