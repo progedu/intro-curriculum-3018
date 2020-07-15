63be0dd4e22f42c7e37f0856afbc2ffd13acafe4
shingowa <ghh11423mama@mbj.nifty.com>
2020-07-15 10:36:16 +0900

    cookieに有効期限、日付表示改良

---
+++ index_old.js
@@ -0,0 +1,12 @@
+'use strict';
+const http = require('http');
+const server = http.createServer((req, res) => {
+  const now = Date.now();
+  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+  res.end(req.headers.cookie);
+});
+const port = 8000;
+server.listen(port, () => {
+  console.info('Listening on ' + port);
+});
+
