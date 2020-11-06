b4de190f6d017a116ecc8d8f26be6e084ce453fb
vagrant <vagrant@vagrant.vm>
2020-11-06 20:02:33 +0900

    有効期限の設定

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const server = http.createServer((req, res) => {
   const now = new Date().getTime();
   res.setHeader('Set-Cookie', `last_access=${now};expires = Mon, 07 Jan 2036 00:00:00 GMT;`);
   const last_access_time = now;
-  res.end(new Date(now).toString(
-
-  ));
+  res.end(new Date(now).toString());
 });
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
 });
-
