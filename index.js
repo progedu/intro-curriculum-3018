3bda994fd28cdac59b42ff5e57ea2816e21ef7bb
natuume <nonegemu3e@gmail.com>
2020-11-06 19:56:52 +0900

    有効期限を設定

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
   res.setHeader('Set-Cookie', 'last_access=' + now + '; expires=Mon, 07 Jan 2036 00:00:00 GMT;');
-  res.end(req.headers.cookie);
+  //res.end(req.headers.cookie);
+  const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+  res.end(new Date(last_access_time).toString());
 });
 const port = 8000;
 server.listen(port, () => {
