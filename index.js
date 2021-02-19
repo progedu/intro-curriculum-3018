5e377a85a47e5d7b1dd3c3cd1f840a93ba1f4274
chainmti <fountain1286@gmail.com>
2021-02-20 07:50:07 +0900

    Cookieに有効期限追加

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   const now = Date.now();
-  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
-  res.end(req.headers.cookie);
+//  res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+  res.setHeader('Set-Cookie', `last_access=${now} ;expires=Mon, 07 Jan 2036 00:00:00 GMT`);
+res.end(req.headers.cookie);
 });
 const port = 8000;
 server.listen(port, () => {
